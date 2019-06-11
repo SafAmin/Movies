f6c439a2df29ae1eb6e2515f9d8187d5b0c5c921
Safa Amin <safa2amin@gail.com>
2019-06-11 16:27:46 +0200

    Added Retrofit APIs and Client

--- app/src/main/java/com/cyberaccounting/movies/network/MoviesAPIs.java
+++ app/src/main/java/com/cyberaccounting/movies/network/MoviesAPIs.java
@@ -1,7 +1,24 @@
 package com.cyberaccounting.movies.network;
 
+import com.cyberaccounting.movies.network.pojo.PopularResponse;
+import com.cyberaccounting.movies.network.pojo.TopRatedResponse;
+import com.cyberaccounting.movies.network.pojo.UpcomingResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by Safa Amin on 11/06/2019.
  */
-public class MoviesAPIs {
+public interface MoviesAPIs {
+
+    @GET("movie/popular")
+    Call<PopularResponse> getPopularMovies(@Query("api_key") String key);
+
+    @GET("movie/top_rated")
+    Call<TopRatedResponse> getTopRatedMovies(@Query("api_key") String key);
+
+    @GET("movie/upcoming")
+    Call<UpcomingResponse> getUpcomingMovies(@Query("api_key") String key);
 }
--- app/src/main/java/com/cyberaccounting/movies/network/MoviesClient.java
+++ app/src/main/java/com/cyberaccounting/movies/network/MoviesClient.java
@@ -1,7 +1,34 @@
 package com.cyberaccounting.movies.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Safa Amin on 11/06/2019.
  */
 public class MoviesClient {
+
+    private static Retrofit retrofit;
+    private static final Object LOCK = new Object();
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    public static Retrofit getRetrofitInstance() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+        if (retrofit == null) {
+            synchronized (LOCK) {
+                retrofit = new retrofit2.Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .client(httpClient.build())
+                        .build();
+            }
+        }
+
+        return retrofit;
+    }
 }
